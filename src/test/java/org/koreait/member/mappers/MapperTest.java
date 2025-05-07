64c084ab99d11b94589f9a2ccb944ba04ffa8d1e
songmikim <ksm0200@gmail.com>
2025-05-07 11:45:39 +0900

    board_table

--- src/test/java/org/koreait/member/mappers/MapperTest.java
+++ src/test/java/org/koreait/member/mappers/MapperTest.java
@@ -3,6 +3,7 @@ package org.koreait.member.mappers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.koreait.global.configs.DBConn;
+import org.koreait.member.entities.Member;
 
 public class MapperTest {
     private MemberMapper mapper;
@@ -14,25 +15,25 @@ public class MapperTest {
 
     @Test
     void test1() {
-//        Member member = new Member();
-//        member.setEmail("user01@test.org");
-//        member.setPassword("12345678");
-//        member.setName("사용자01");
-//        member.setTerms(true);
-//        member.setMobile("01034812101");
-//
-//        int affectedRows = mapper.register(member);
-//        System.out.println(affectedRows);
+        Member member = new Member();
+        member.setEmail("user01@test.org");
+        member.setPassword("12345678");
+        member.setName("사용자01");
+        member.setTerms(true);
+        member.setMobile("01034812101");
 
-//        Member member = mapper.get("user01@test.org").orElse(null);
-//        System.out.println(member);
+        int affectedRows = mapper.register(member);
+        System.out.println(affectedRows);
+
+        //Member member = mapper.get("user01@test.org").orElse(null);
+        //System.out.println(member);
 
 //        Member member = new Member();
 //        member.setEmail("user01@test.org");
 //        member.setName("(수정)사용자01");
 //        mapper.update(member);
 
-        mapper.delete("user01@test.org");
+       // mapper.delete("user01@test.org");
 
         //List< Member> members = mapper.getList();
         //members.forEach(System.out::println);
